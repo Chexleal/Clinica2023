9dc23c595eb9554620adbe39d49006639c20640a
Melissa339 <dulceminera9@gmail.com>
2023-03-30 16:30:55 -0600

    Back para l abusqueda de usuarios

--- 3 Application/ClinicaServices/UserServices.cs
+++ 3 Application/ClinicaServices/UserServices.cs
@@ -6,6 +6,9 @@ public interface IUserServices
 {
 	bool Authenticate(string user, string password);
 	List<Usuario> GetAll();
+	void AddUser(Usuario usuario);
+	List<Usuario> SearchUser(string input);
+    Usuario GetUsuario(Guid id);
 }
 public class UserServices : IUserServices
 {
@@ -15,7 +18,13 @@ public class UserServices : IUserServices
 		_dbContext = dbContext;
 	}
 
-	public bool Authenticate(string user, string password)
+    public void AddUser(Usuario usuario)
+    {
+			_dbContext.Usuarios.Add(usuario);
+			_dbContext.SaveChanges();
+    }
+
+    public bool Authenticate(string user, string password)
 	{
 		var userItem= _dbContext.Usuarios.FirstOrDefault(x=>x.Password == password && x.NombreUsuario==user);
 		if (userItem == null) return false;
@@ -26,4 +35,42 @@ public class UserServices : IUserServices
 	{
 		return _dbContext.Usuarios.ToList();
 	}
+
+    public Usuario GetUsuario(Guid id)
+    {
+        return _dbContext.Usuarios.Find(id);
+    }
+
+    public List<Usuario> SearchUser(string input)
+    {
+        List<Usuario> result = new List<Usuario>();
+        //List<Guid> guids = new List<Guid>();
+		foreach (var user in _dbContext.Usuarios.ToList())
+		{
+            if (user.NombreUsuario.Contains(input) ||
+                user.NombreUsuario.Contains(input) ||
+                user.PreguntaSeg.Contains(input) ||
+                user.RespuestaSeg.Contains(input) ||
+                user.Dpi.Contains(input) ||
+                user.Nombre.Contains(input) ||
+                user.Apellido.Contains(input) ||
+                user.FechaNacimiento.ToString().Contains(input) ||
+                user.Telefono.ToString().Contains(input) ||
+                user.EstadoCivil.Contains(input) ||
+                user.Profesion.Contains(input) ||
+                user.Nacionalidad.Contains(input) ||
+                user.Remitido.Contains(input) ||
+                user.Antecedentes.Contains(input) ||
+                user.TipoSange.Contains(input) ||
+                user.NoRegistro.Contains(input) ||
+                user.Password.Contains(input)
+                )
+                result.Add(_dbContext.Usuarios.Find(user.IdUsuario));
+				//guids.Add(user.IdUsuario);
+        }
+
+
+
+        return result;
+    }
 }
---
+++ 4 Web/clinicamhsystem/Controllers/UsuariosController.cs
@@ -0,0 +1,129 @@
+﻿using ClinicaDomain;
+using clinicamhsystem.Models;
+using ClinicaServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace clinicaWeb.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly IUserServices _userServices;
+
+        public UsuariosController(IUserServices userServices)
+        {
+            _userServices = userServices;
+        }
+
+        // GET: UsuariosController
+        public ActionResult Index()
+        {
+            var users = _userServices.GetAll();
+            return View(users);
+        }
+
+        // GET: UsuariosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuariosController/Create
+        public ActionResult Create()
+        {   
+            return View();
+        }
+
+        // POST: UsuariosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                Guid id = Guid.NewGuid();
+                Usuario usuario = new Usuario();
+                usuario.IdUsuario = id;
+                usuario.NombreUsuario = collection["NombreUsuario"];
+                usuario.PreguntaSeg = collection["PreguntaSeg"];
+                usuario.RespuestaSeg = collection["RespuestaSeg"];
+                usuario.Dpi = collection["Dpi"];
+                usuario.Nombre = collection["Nombre"];
+                usuario.Apellido = collection["Apellido"];
+                usuario.FechaNacimiento = DateTime.Parse(collection["FechaNacimiento"]);
+                usuario.Telefono = Int32.Parse(collection["Telefono"]);
+                usuario.Correo = collection["Correo"];
+                usuario.EstadoCivil = collection["EstadoCivil"];
+                usuario.Profesion = collection["Profesion"];
+                usuario.Nacionalidad = collection["Nacionalidad"];
+                usuario.Remitido = collection["Remitido"];
+                usuario.Antecedentes = collection["Antecedentes"];
+                usuario.TipoSange = collection["TipoSange"];
+                usuario.NoRegistro = collection["NoRegistro"];
+                usuario.Password = collection["Password"];
+                usuario.EstadoEliminado = false;
+                usuario.UsuarioActivo = true;
+
+              
+                _userServices.AddUser(usuario);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Search(string input)
+        {
+            var idResult = _userServices.SearchUser(input);
+            if (idResult == null)
+                return View("Index");
+            else
+                return View(idResult);
+        }
+
+        // POST: UsuariosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

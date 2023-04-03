a696afd6546b35b42384d2979724b0de2a883ec0
EDGAR JOSE PABLO LEAL HERNANDEZ <chexleal@gmail.com>
2023-04-03 00:41:38 -0600

    Cambios al código

--- 3 Application/ClinicaServices/UserServices.cs
+++ 3 Application/ClinicaServices/UserServices.cs
@@ -1,58 +1,59 @@
 ﻿using ClinicaDomain;
-
-using Microsoft.EntityFrameworkCore;
+using System;
 using System.Net.Mail;
 using System.Net;
+using PaaS.Framework.Utils.Extensions;
+using ServiceStack;
 
 namespace ClinicaServices;
 
 public interface IUserServices
 {
-	bool Authenticate(string user, string password);
-  
+    bool Authenticate(string user, string password);
+
     bool CheckUserExist(string userName);
     string? CheckAnswer(string userName);
     string? SecurityQuestion(string userName);
     List<Usuario> GetAll();
-  	void AddUser(Usuario user);
-	List<Usuario> SearchUser(string input);
+    void AddUser(Usuario user);
+    List<Usuario> SearchUser(string input);
     Usuario GetUser(Guid id);
     void DeleteUser(Guid id);
     void UpdateUser(Usuario user);
 }
 public class UserServices : IUserServices
 {
-	private readonly ClinicaContext _dbContext;
-	public UserServices(ClinicaContext dbContext)
-	{
-		_dbContext = dbContext;
-	}
+    private readonly ClinicaContext _dbContext;
+    public UserServices(ClinicaContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
 
     public void AddUser(Usuario user)
     {
-			_dbContext.Usuarios.Add(user);
-			_dbContext.SaveChanges();
+        user.IdUsuario = $"{user.NombreUsuario.Trim().ToLower()}".ToGuid();
+        user.EstadoEliminado = false;
+        user.UsuarioActivo = true;
+        _dbContext.Usuarios.Add(user);
+        _dbContext.SaveChanges();
     }
 
     public bool Authenticate(string user, string password)
-	{
-		var userItem= _dbContext.Usuarios.FirstOrDefault(x=>x.Password == password && x.NombreUsuario==user);
-		if (userItem == null) return false;
-		return true;
-	}
+    {
+        var userItem = _dbContext.Usuarios.FirstOrDefault(x => x.Password == password && x.NombreUsuario == user);
+        if (userItem is null) return false;
+        return true;
+    }
 
-	public List<Usuario> GetAll()
-	{
-		return _dbContext.Usuarios.ToList();
-	}
+    public List<Usuario> GetAll()
+    {
+        return _dbContext.Usuarios.ToList();
+    }
 
     public bool RecoverAccount(string email)
     {
         // Check if the email is registered in the database
-        if (!CheckUserExist(email))
-        {
-            return false;
-        }
+        if (!CheckUserExist(email)) return false;
 
         // Generate a new password and update the user's account with it
         string newPassword = GeneratePassword();
@@ -72,10 +73,10 @@ public class UserServices : IUserServices
 
         try
         {
-            using (MailMessage mail = new MailMessage(fromAddress, toAddress, subject, body))
+            using (MailMessage mail = new(fromAddress, toAddress, subject, body))
             {
                 mail.IsBodyHtml = false;
-                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                using (SmtpClient smtp = new("smtp.gmail.com", 587))
                 {
                     smtp.EnableSsl = true;
                     smtp.UseDefaultCredentials = false;
@@ -99,28 +100,26 @@ public class UserServices : IUserServices
         // Check if the email is registered in the database
         // Return true if it exists, false otherwise
         var userItem = _dbContext.Usuarios.FirstOrDefault(x => x.NombreUsuario == userName);
-        if (userItem == null) return false;
+        if (userItem is null) return false;
         return true;
     }
 
     public string? SecurityQuestion(string userName)
     {
         var userItem = _dbContext.Usuarios.FirstOrDefault(x => x.NombreUsuario.ToLower().Trim() == userName.ToLower().Trim());
-        if (userItem == null) {
-
+        if (userItem is null)
             return null;
-        }
         else
         {
             var preguntaSegura = userItem.PreguntaSeg.ToString();
             return preguntaSegura;
-        } 
+        }
     }
 
     public string? CheckAnswer(string answer)
     {
         var userItem = _dbContext.Usuarios.FirstOrDefault(x => x.RespuestaSeg.ToLower().Trim() == answer.ToLower().Trim());
-        if (userItem != null) return userItem.RespuestaSeg.ToString();
+        if (userItem is not null) return userItem.RespuestaSeg.ToString();
         return null;
     }
 
@@ -145,9 +144,9 @@ public class UserServices : IUserServices
 
     public void UpdateUser(Usuario user)
     {
-        if (user != null)
-        {
-            var userDB = GetUser(user.IdUsuario);
+        var userDB = GetUser(user.IdUsuario);
+        if (userDB is not null)
+        {       
             userDB.NombreUsuario = user.NombreUsuario;
             userDB.PreguntaSeg = user.PreguntaSeg;
             userDB.RespuestaSeg = user.RespuestaSeg;
@@ -170,7 +169,7 @@ public class UserServices : IUserServices
     public void DeleteUser(Guid id)
     {
         var user = GetUser(id);
-        if (user != null)
+        if (user is not null)
         {
             user.EstadoEliminado = true;
             _dbContext.SaveChanges();
@@ -179,30 +178,51 @@ public class UserServices : IUserServices
 
     public List<Usuario> SearchUser(string input)
     {
-        List<Usuario> result = new List<Usuario>();
-        if (string.IsNullOrEmpty(input)) { return null; }
-		foreach (var user in _dbContext.Usuarios.ToList())
-		{
-            if (user.NombreUsuario.Contains(input) ||
-                user.NombreUsuario.Contains(input) ||
-                user.PreguntaSeg.Contains(input) ||
-                user.RespuestaSeg.Contains(input) ||
-                user.Dpi.Contains(input) ||
-                user.Nombre.Contains(input) ||
-                user.Apellido.Contains(input) ||
-                user.FechaNacimiento.ToString().Contains(input) ||
-                user.Telefono.ToString().Contains(input) ||
-                user.EstadoCivil.Contains(input) ||
-                user.Profesion.Contains(input) ||
-                user.Nacionalidad.Contains(input) ||
-                user.Remitido.Contains(input) ||
-                user.Antecedentes.Contains(input) ||
-                user.TipoSange.Contains(input) ||
-                user.NoRegistro.ToString().Contains(input) ||
-                user.Password.Contains(input)
-                )
-                result.Add(GetUser(user.IdUsuario));
-        }
+
+        if (input.IsNullOrEmpty()) return null;
+        
+        //DE ESTA FORMA NO SE NECESITA IR POR TODOS LOS USUARIOS, SINO SE FILTRAN DIRECTOS EN DB, LO CUAL LO HACE MAS RÁPIDO
+        List<Usuario> result = _dbContext.Usuarios.Where(x => 
+        x.NombreUsuario.Contains(input) ||
+        x.NombreUsuario.Contains(input) ||
+        x.PreguntaSeg.Contains(input) ||
+        x.RespuestaSeg.Contains(input) ||
+        x.Dpi.Contains(input) ||
+        x.Nombre.Contains(input) ||
+        x.Apellido.Contains(input) ||
+        x.FechaNacimiento.ToString().Contains(input) ||
+        x.Telefono.ToString().Contains(input) ||
+        x.EstadoCivil.Contains(input) ||
+        x.Profesion.Contains(input) ||
+        x.Nacionalidad.Contains(input) ||
+        x.Remitido.Contains(input) ||
+        x.Antecedentes.Contains(input) ||
+        x.TipoSange.Contains(input) ||
+        x.NoRegistro.ToString().Contains(input) ||
+        x.Password.Contains(input)).ToList();
+
+        //      foreach (var user in _dbContext.Usuarios.ToList())
+        //{
+        //          if (user.NombreUsuario.Contains(input) ||
+        //              user.NombreUsuario.Contains(input) ||
+        //              user.PreguntaSeg.Contains(input) ||
+        //              user.RespuestaSeg.Contains(input) ||
+        //              user.Dpi.Contains(input) ||
+        //              user.Nombre.Contains(input) ||
+        //              user.Apellido.Contains(input) ||
+        //              user.FechaNacimiento.ToString().Contains(input) ||
+        //              user.Telefono.ToString().Contains(input) ||
+        //              user.EstadoCivil.Contains(input) ||
+        //              user.Profesion.Contains(input) ||
+        //              user.Nacionalidad.Contains(input) ||
+        //              user.Remitido.Contains(input) ||
+        //              user.Antecedentes.Contains(input) ||
+        //              user.TipoSange.Contains(input) ||
+        //              user.NoRegistro.ToString().Contains(input) ||
+        //              user.Password.Contains(input)
+        //              )
+        //              result.Add(GetUser(user.IdUsuario));
+        //      }
 
         return result;
     }

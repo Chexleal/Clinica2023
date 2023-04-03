1c39b5464d22af34f7b67ebe4b551027929c1016
EDGAR JOSE PABLO LEAL HERNANDEZ <chexleal@gmail.com>
2023-04-03 01:24:10 -0600

    DataTable en usuarios

---
+++ 4 Web/clinicamhsystem/wwwroot/js/pages/usuarios.js
@@ -0,0 +1,73 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    CreateTable();
+});
+
+function CreateTable() {
+    $('#tableUser').DataTable({
+        "autoWidth": false,
+        "ordering": true,
+        "lengthChange": true,
+        dom: 'Bfrtip',
+        "pageLength": 20,
+        "language": {
+            searchPlaceholder:'Buscar ...',
+            sSearch: '',
+            lengthMenu: 'MENU items/page',
+            paginate: {
+                previous: 'Anterior',
+                next: 'Siguiente',
+            },
+            info: "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            infoEmpty: "Mostrando 0 a 0 de 0 registros",
+            infoFiltered: "(Filtrado de _TOTAL_ registros)",
+            processing: `<div class="progress" style="margin: 0; width: 100%">
+                                      <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div>
+                                    </div>`,
+            emptyTable: "No hay datos disponibles en esta tabla",
+            buttons: {
+                copyTitle: "Copiar al portapapeles",
+                copySuccess: {
+                    1: "Copi&oacute; una fila al portapapeles",
+                    _: "Se copiaron %d filas al portapapeles"
+                },
+            }
+        },
+        buttons: [
+            {
+                extend: 'copy',
+                text: '<i class="fas fa-clone"></i><strong>Copiar</strong>',
+                messageTop: '',
+                className: "text-dark btn-light",
+                title: "Usuarios",
+                filename: "Usuarios",
+                exportOptions: {
+                    columns: [0,1,2,3,4,5,6,7],
+                    page: 'all'
+                },
+                orientation: "landscape",
+                pageSize: "LEGAL"
+            },
+            {
+                extend: 'excel',
+                text: '<i class="fas fa-file-excel"></i><strong>Excel </strong>',
+                messageTop: '',
+                className: "text-dark btn-light",
+                title: "Usuarios",
+                filename: "Usuarios",
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4, 5, 6, 7],
+                    modifier: {
+                        page: 'all',
+                        search: 'none'
+                    }
+                },
+                orientation: "landscape",
+                pageSize: "LEGAL"
+            }]
+    });
+}
